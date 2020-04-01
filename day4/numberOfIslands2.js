41552d97ade45f77c52f2b11587391ad25c72c6f
Jacob Richards <jacob.richards33@gmail.com>
2020-03-31 22:36:31 -0700

    done

--- day4/numberOfIslands2.js
+++ day4/numberOfIslands2.js
@@ -1,63 +1,73 @@
 const { test } = require('../common/utils');
 
-const xyToString = (x, y) => {
-  return `${x},${y}`;
+const isInBounds = (i, j, grid) => {
+  return i >= 0 && j >= 0 && i < grid.length && j < grid[0].length;
 };
 const numIslands = grid => {
-  const visited = new Set();
-  const explore = (x, y, grid) => {
-    // visit the whole island
-    if (
-      visited.has(indexString) ||
-      x < 0 ||
-      y < 0 ||
-      x >= grid.length ||
-      y >= grid[0].length
-    )
-      return;
-
-    visited.add(xyToString(x, y));
-
-    recurse(x + 1, y, grid);
-    recurse(x + 1, y, grid);
-    recurse(x, y + 1, grid);
-    recurse(x + 1, y, grid);
-  };
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      if (!visited.has(xyToString(i, j))) {
-        explore(i, j, grid);
-      }
+  const explore = (i, j) => {
+    if (isInBounds(i, j, grid) && grid[i][j] == '1') {
+      grid[i][j] = '0'; // if the current position is a 1 and within bounds,
+      explore(i + 1, j); // explore the 4 adjacent positions (up, down, left, right)
+      explore(i - 1, j);
+      explore(i, j + 1);
+      explore(i, j - 1);
     }
-  }
+  };
+  let numberOfIslands = 0;
+  grid.forEach((row, i) => {
+    // look at every entry in the grid
+    row.forEach((_, j) => {
+      if (grid[i][j] === '1') {
+        // if we find land, explore it
+        explore(i, j);
+        numberOfIslands++;
+      }
+    });
+  });
+  return numberOfIslands;
 };
 
 const testArr = [
   [
     [
-      [1, 1, 1, 1, 0],
-      [1, 1, 0, 1, 0],
-      [1, 1, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+      ['1', '1', '1', '1', '0'],
+      ['1', '1', '0', '1', '0'],
+      ['1', '1', '0', '0', '0'],
+      ['0', '0', '0', '0', '0']
     ]
   ],
   [
     [
-      [1, 1, 0, 0, 0],
-      [1, 1, 0, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 0, 0, 1, 1]
+      ['1', '1', '0', '0', '0'],
+      ['1', '1', '0', '0', '0'],
+      ['0', '0', '1', '0', '0'],
+      ['0', '0', '0', '1', '1']
     ]
   ],
   [
     [
-      [1, 0, 1, 1, 0],
-      [1, 1, 0, 1, 0],
-      [0, 1, 0, 1, 0],
-      [0, 0, 0, 1, 1]
+      ['1', '0', '1', '1', '0'],
+      ['1', '1', '0', '1', '0'],
+      ['0', '1', '0', '1', '0'],
+      ['0', '0', '0', '1', '1']
+    ]
+  ],
+  [
+    [
+      ['1', '1', '1', '1', '0'],
+      ['1', '1', '0', '1', '0'],
+      ['1', '1', '0', '0', '0'],
+      ['0', '0', '0', '0', '0']
+    ]
+  ],
+  [
+    [
+      ['1', '1', '1'],
+      ['0', '1', '0'],
+      ['1', '1', '1']
     ]
   ]
 ];
-const expectedArr = [1, 3, 2];
+const expectedArr = [1, 3, 2, 1, 1];
 
-test(numIslands, (a, b) => a === b, [testArr[1]], [expectedArr[1]]);
+test(numIslands, (a, b) => a === b, testArr, expectedArr);
